2b2e60747b9c2a96f3e64c008617bd941a406147
Chloe Adler-Mandile <chloe.mandile@gmail.com>
2019-04-04 11:57:15 -0400

    up to date on lab2

---
+++ ece3849_lab2_cadlermandile_skalimani_smspry/buttons.h
@@ -0,0 +1,85 @@
+/*
+ * buttons.h
+ *
+ *  Created on: Aug 12, 2012, modified 9/8/2017
+ *      Author: Gene Bogdanov
+ *
+ *  Button debouncer, calibrated for 200 samples/sec.
+ */
+
+#ifndef BUTTONS_H_
+#define BUTTONS_H_
+
+#include <stdint.h>
+
+#define BUTTON_COUNT 5				// number of buttons excluding joystick directions
+#define BUTTON_AND_JOYSTICK_COUNT 9 // number of buttons including joystick directions
+#define BUTTON_SAMPLES_PRESSED 2	// number of samples before a button is considered pressed
+#define BUTTON_SAMPLES_RELEASED 5	// number of samples before a button is considered released
+// counter value indicating button pressed state
+#define BUTTON_PRESSED_STATE (BUTTON_SAMPLES_RELEASED*BUTTON_SAMPLES_PRESSED)
+#define BUTTON_STATE_INCREMENT (BUTTON_PRESSED_STATE/BUTTON_SAMPLES_PRESSED)
+#define BUTTON_STATE_DECREMENT (BUTTON_PRESSED_STATE/BUTTON_SAMPLES_RELEASED)
+
+#define BUTTON_SCAN_RATE 200    // [Hz] button scanning interrupt rate
+#define BUTTON_INT_PRIORITY 32  // button interrupt priority (higher number is lower priority)
+
+#define BUTTON_AUTOREPEAT_INITIAL 100   // how many samples must read pressed before autorepeat starts
+#define BUTTON_AUTOREPEAT_NEXT 10       // how many samples must read pressed before the next repetition
+
+#define JOYSTICK_UPPER_PRESS_THRESHOLD 3595     // above this ADC value, button is pressed
+#define JOYSTICK_UPPER_RELEASE_THRESHOLD 3095   // below this ADC value, button is released
+#define JOYSTICK_LOWER_PRESS_THRESHOLD 500      // below this ADC value, button is pressed
+#define JOYSTICK_LOWER_RELEASE_THRESHOLD 1000   // above this ADC value, button is released
+
+#define ADC_SAMPLING_RATE 1000000   // [samples/sec] desired ADC sampling rate
+#define CRYSTAL_FREQUENCY 25000000  // [Hz] crystal oscillator frequency used to calculate clock rates
+#define pll_frequency 480000000  // [Hz] crystal oscillator frequency used to calculate clock rates
+
+#define ADC_BUFFER_SIZE 2048 // size must be a power of 2
+#define BUFFER_COPY_SIZE 1024
+#define ADC_BUFFER_WRAP(i) ((i) & (ADC_BUFFER_SIZE - 1)) // index wrapping macro
+//#define ADC_OFFSET 1260
+#define ADC_OFFSET 2840
+#define VIN_RANGE 3.3
+#define PIXELS_PER_DIV 20
+#define ADC_BITS 12
+
+#define FIFO_SIZE 11        // FIFO capacity is 1 item fewer
+extern volatile char fifo[FIFO_SIZE];  // FIFO storage array
+extern volatile int fifo_head; // index of the first item in the FIFO
+extern volatile int fifo_tail; // index one step past the last item
+
+extern char c;
+
+extern volatile uint32_t gButtons;	// debounced button state, one per bit in the lowest bits
+extern uint32_t gJoystick[2];       // joystick coordinates
+
+extern volatile int32_t gADCBufferIndex;
+extern uint32_t gADCSamplingRate;   // [Hz] actual ADC sampling rate
+volatile int16_t gADCBuffer[ADC_BUFFER_SIZE]; // circular buffer
+volatile int16_t gWaveformBuffer[BUFFER_COPY_SIZE];
+volatile int32_t triggerBuffer[3];
+volatile uint32_t gADCErrors; // number of missed ADC deadlines
+
+
+// initialize all button and joystick handling hardware
+void ButtonInit(void);
+
+// update the debounced button state in the global variable gButtons
+// the input argument is a bitmap of raw button state from the hardware
+void ButtonDebounce(uint32_t buttons);
+
+// sample joystick and convert to button presses
+void ButtonReadJoystick(void);
+
+void ADC_ISR(void);
+
+// autorepeat button presses if a button is held long enough
+uint32_t ButtonAutoRepeat(void);
+
+int fifo_put(char data);
+int fifo_get(char *data);
+
+
+#endif /* BUTTONS_H_ */
---
+++ ece3849_lab2_cadlermandile_skalimani_smspry/main.c
@@ -0,0 +1,292 @@
+/*
+ * ECE 3849 Lab2 starter project
+ *
+ * Gene Bogdanov    9/13/2017
+ */
+/* XDCtools Header files */
+#include <xdc/std.h>
+#include <xdc/runtime/System.h>
+#include <xdc/cfg/global.h>
+
+/* BIOS Header files */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/interrupt.h"
+
+uint32_t gSystemClock = 120000000; // [Hz] system clock frequency
+
+/*
+ *  ======== main ========
+ */
+
+int main(void)
+{
+    IntMasterDisable();
+
+    // hardware initialization goes here
+    Crystalfontz128x128_Init(); // Initialize the LCD display driver
+    Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP); // set screen orientation
+    tContext sContext;
+    GrContextInit(&sContext, &g_sCrystalfontz128x128); // Initialize the grlib graphics context
+    GrContextFontSet(&sContext, &g_sFontFixed6x8); // select font
+    ButtonInit();
+
+    /* Start BIOS */
+    BIOS_start();
+
+    return (0);
+}
+
+void WaveformTask(UArg arg1, UArg arg2) //high priority
+{
+    //pend on the button Mailbox (blocking call). When it
+    //receives a button ID, it should modify the oscilloscope
+    //settings and signal the Display Task (using a Semaphore)
+    //to update the LCD screen.
+
+
+
+    IntMasterEnable();
+
+    while (true) {
+        // do nothing
+    }
+}
+
+void UserInputTask(UArg arg1, UArg arg2) //medium priority
+{
+    IntMasterEnable();
+
+    while (true) {
+        // do nothing
+    }
+}
+
+void DisplayTask(UArg arg1, UArg arg2) //low priority
+{
+    IntMasterEnable();
+
+    while (true) {
+        // do nothing
+    }
+}
+//copied from Lab1
+
+//#include <stdint.h>
+//#include <stdbool.h>
+//#include "driverlib/fpu.h"
+//#include "driverlib/sysctl.h"
+//#include "driverlib/interrupt.h"
+//#include "Crystalfontz128x128_ST7735.h"
+//#include <stdio.h>
+//#include "buttons.h"
+//#include "driverlib/timer.h"
+//#include "inc/hw_memmap.h"
+//
+//uint32_t gSystemClock; // [Hz] system clock frequency
+//volatile uint32_t gTime = 8345; // time in hundredths of a second
+//
+//// temporary buffer storage variables
+//volatile int32_t temp0, temp1;
+//volatile int32_t gTriggerIndex;
+//volatile int i, j, k, l, n, m, q;
+//
+//// data to display conversion variables
+//uint16_t sample;
+//volatile float fVoltsPerDiv = 1.0;
+//float fScale;
+//int16_t y[1024];
+//
+////
+//char buttonRead;
+//char getTest;
+//
+//char p;
+//char str[50];
+//
+//uint32_t count_unloaded = 0;
+//uint32_t count_loaded = 0;
+//float cpu_load = 0.0;
+//
+//
+////for interpreting gButtons and putting into FIFO
+//uint32_t buttons; //local copy of gButtons
+//
+////triggerslope flag
+//int triggerSlope =  0;
+//
+//void buttonChar(uint32_t buttons);
+//uint32_t cpu_load_count(void);
+//
+//int main(void)
+//{
+//
+//    IntMasterDisable();
+//
+//    // Enable the Floating Point Unit, and permit ISRs to use it
+//    FPUEnable();
+//    FPULazyStackingEnable();
+//
+//    // Initialize the system clock to 120 MHz
+//    gSystemClock = SysCtlClockFreqSet(SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480, 120000000);
+//
+//    Crystalfontz128x128_Init(); // Initialize the LCD display driver
+//    Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP); // set screen orientation
+//
+//    tContext sContext;
+//    GrContextInit(&sContext, &g_sCrystalfontz128x128); // Initialize the grlib graphics context
+//    GrContextFontSet(&sContext, &g_sFontFixed6x8); // select font
+//    ButtonInit();
+//
+//    count_unloaded = cpu_load_count();
+//
+//    IntMasterEnable();
+//
+//    j = 0;
+//    i = 0;
+//
+//    tRectangle rectFullScreen = {0, 0, GrContextDpyWidthGet(&sContext)-1, GrContextDpyHeightGet(&sContext)-1};
+//    GrContextForegroundSet(&sContext, ClrBlack);
+//    GrRectFill(&sContext, &rectFullScreen); // fill screen with black
+//
+//    while(1){
+//
+//        if (fifo_get(&p)) {
+//
+//            /* Joystick configuration for scaling   */
+//
+//            switch (p) {
+//            case 'R':               // if right joystick selected
+//                fVoltsPerDiv = 0.1;
+//                break;
+//            case 'L':              // if left joystick is selected
+//                fVoltsPerDiv = 0.2;
+//                break;
+//            case 'U':              // if up joystick is selected
+//                fVoltsPerDiv = 0.5;
+//                break;
+//            case 'D':              // if down joystick is selected
+//                fVoltsPerDiv = 1.0;
+//                break;
+//            case '1':
+//                triggerSlope =! triggerSlope;
+//                break;
+//            }
+//        }
+//
+//        fScale = (VIN_RANGE * PIXELS_PER_DIV)/((1 << ADC_BITS) * fVoltsPerDiv);
+//
+//        GrContextForegroundSet(&sContext, ClrBlack);
+//        GrRectFill(&sContext, &rectFullScreen); // fill screen with black
+//
+//
+//        //draw grid
+//        GrContextForegroundSet(&sContext, ClrBlue); // blue lines
+//        for(m=-16; m<LCD_HORIZONTAL_MAX; m+=PIXELS_PER_DIV){
+//            for(q=-16; q<LCD_VERTICAL_MAX; q+=PIXELS_PER_DIV){
+//                GrLineDraw(&sContext, m, q, m+PIXELS_PER_DIV, q);
+//                GrLineDraw(&sContext, m, q, m, q+PIXELS_PER_DIV);
+//            }
+//        }
+//
+//
+//        // store values from gADCBuffer to local variables and check for slope
+//        gTriggerIndex = gADCBufferIndex - LCD_HORIZONTAL_MAX/2;
+//        int cur_sample, last_sample;
+//        last_sample = gADCBuffer[ADC_BUFFER_WRAP(gTriggerIndex)];
+//
+//        if (triggerSlope) {
+//            for (j = 0; j < ADC_BUFFER_SIZE/2; j++) {
+//                cur_sample = gADCBuffer[ADC_BUFFER_WRAP(gTriggerIndex--)];
+//                if (cur_sample <= ADC_OFFSET && last_sample >= ADC_OFFSET) {
+//                    // found falling trigger
+//                    break;
+//                }
+//                last_sample = cur_sample;
+//            }
+//        }
+//        else {
+//
+//            for (j = 0; j < ADC_BUFFER_SIZE/2; j++) {
+//                cur_sample = gADCBuffer[ADC_BUFFER_WRAP(gTriggerIndex--)];
+//                if (cur_sample >= ADC_OFFSET && last_sample <= ADC_OFFSET) {
+//                    // found rising trigger
+//                    break;
+//                }
+//                last_sample = cur_sample;
+//            }
+//
+//        }
+//
+//        if (j >= ADC_BUFFER_SIZE/2) {
+//            gTriggerIndex = gADCBufferIndex - LCD_HORIZONTAL_MAX/2;
+//        }
+//
+//        for (j = 0; j < LCD_HORIZONTAL_MAX; j++) {
+//            gWaveformBuffer[j] = gADCBuffer[ADC_BUFFER_WRAP(gTriggerIndex + j - LCD_HORIZONTAL_MAX/2)];
+//        }
+//
+//        j = 0;
+//
+//        // Print time scale
+//        snprintf(str, sizeof(str), "20 us");
+//        GrContextForegroundSet(&sContext, ClrWhite);
+//        GrStringDraw(&sContext, str, /*length*/ -1, /*x*/ 0, /*y*/ 3, /*opaque*/ false);
+//
+//        // Print volts per division
+//        snprintf(str, sizeof(str), "%.1f V", fVoltsPerDiv);
+//        GrContextForegroundSet(&sContext, ClrWhite);
+//        GrStringDraw(&sContext, str, /*length*/ -1, /*x*/ 50, /*y*/ 3, /*opaque*/ false);
+//
+//        if(triggerSlope){ //falling edge
+//            GrContextForegroundSet(&sContext, ClrWhite);
+//            GrLineDraw(&sContext,115, 3, 120, 3);
+//            GrLineDraw(&sContext,115, 3, 115, 8);
+//            GrLineDraw(&sContext,110, 8, 115, 8);
+//        }
+//        else{ //rising edge
+//            GrContextForegroundSet(&sContext, ClrWhite);
+//            GrLineDraw(&sContext,110, 3, 115, 3);
+//            GrLineDraw(&sContext,115, 3, 115, 8);
+//            GrLineDraw(&sContext,115, 8, 120, 8);
+//        }
+//
+//        //draw waveform
+//        GrContextForegroundSet(&sContext, ClrYellow);
+//
+//        for(n = 0; n<LCD_HORIZONTAL_MAX;n++){
+//            sample = gWaveformBuffer[n];
+//            y[n] = LCD_VERTICAL_MAX/2 -(int)(fScale * ((int)sample - ADC_OFFSET));
+//
+//            if (n != 0){
+//                GrLineDraw(&sContext, n-1, y[n-1], n, y[n]);
+//            }
+//        }
+//
+//        count_loaded = cpu_load_count();
+//        cpu_load = 1.0f - (float)count_loaded/count_unloaded; // compute CPU load
+//        snprintf(str, sizeof(str), "CPU Load = %.2f %%", cpu_load*(100));
+//        GrContextForegroundSet(&sContext, ClrWhite);
+//        GrStringDraw(&sContext, str, /*length*/ -1, /*x*/ 0, /*y*/ 118, /*opaque*/ false);
+//
+//        GrFlush(&sContext); // flush the frame buffer to the LCD
+//    }
+//}
+//
+///*
+// * CPU load count
+// * Uses Timer3
+// */
+//uint32_t cpu_load_count(void)
+//{
+//    uint32_t i = 0;
+//    TimerIntClear(TIMER3_BASE, TIMER_TIMA_TIMEOUT);
+//    TimerEnable(TIMER3_BASE, TIMER_A); // start one-shot timer
+//    while (!(TimerIntStatus(TIMER3_BASE, false) & TIMER_TIMA_TIMEOUT))
+//        i++;
+//    return i;
+//}
+
